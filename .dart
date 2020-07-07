b243d4c3d997b37b2d23f8d0b15d1e2c6113180a
MarvinRem <67871529+MarvinRem@users.noreply.github.com>
2020-07-07 11:49:21 +0200

    Create .dart

---
+++ .dart
@@ -0,0 +1,17 @@
+main(){
+  int zahl = 1;
+  int zahl2 = 9;
+  
+  print(zahl);
+  print(zahl);
+  
+  double kommazahl = 1.02;
+  double kommazahl2 = 1.025;
+  
+  print(kommazahl);
+  print(kommazahl2);
+  
+  String hallo = 'Hallo';
+  
+  print(hallo);
+}
